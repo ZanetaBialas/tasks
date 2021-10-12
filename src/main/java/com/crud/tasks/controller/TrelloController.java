7cb896992469f11c8719cf124d7c188b73e4ad61
unknown <zanetabialas1987@gmail.com>
2021-10-12 22:07:47 +0200

    Added module 23.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,6 +3,7 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.service.TrelloService;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,9 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class TrelloController {
 
-    private final TrelloClient trelloClient;
+   // private final TrelloClient trelloClient;
+
+    private final TrelloService trelloService;
 
    // @GetMapping("getTrelloBoards")
    // public void getTrelloBoards() {
@@ -34,13 +37,22 @@ public class TrelloController {
    //     });
    // }
 
+    //@GetMapping("getTrelloBoards")
+    //public List<TrelloBoardDto> getTrelloBoards(){
+    //    return  trelloClient.getTrelloBoards();
+    //}
     @GetMapping("getTrelloBoards")
-    public List<TrelloBoardDto> getTrelloBoards(){
-        return  trelloClient.getTrelloBoards();
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloService.fetchTrelloBoards();
     }
 
+    //@PostMapping("createTrelloCard")
+    //public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+    //    return trelloClient.createNewCard(trelloCardDto);
+    //}
+
     @PostMapping("createTrelloCard")
-    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        return trelloClient.createNewCard(trelloCardDto);
+    public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloService.createTrelloCard(trelloCardDto);
     }
 }
